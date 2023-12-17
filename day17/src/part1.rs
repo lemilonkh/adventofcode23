f623972c3f789856f93f15dee13486eae9936a84
Milan Gruner <milangruner@gmail.com>
2023-12-17 15:30:30 +0100

    Solve day 17 part 1
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- day17/src/part1.rs
+++ day17/src/part1.rs
@@ -1,10 +1,10 @@
 use array2d::{Array2D, Error};
+use pathfinding::directed::astar::astar;
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
-use pathfinding::prelude::astar;
 use Direction::*;
 
-#[derive(Debug, PartialEq, EnumIter, Eq, Clone)]
+#[derive(Debug, PartialEq, EnumIter, Eq, Clone, Copy, Hash, PartialOrd, Ord)]
 enum Direction {
     NORTH,
     EAST,
@@ -30,37 +30,66 @@ impl Direction {
             WEST => EAST,
         }
     }
+
+    fn char(&self) -> char {
+        match self {
+            NORTH => '^',
+            EAST => '>',
+            SOUTH => 'v',
+            WEST => '<',
+        }
+    }
 }
 
-#[derive(Debug, Clone, Copy, Eq, Hash, PartialEq, PartialOrd, Ord)]
-struct Pos(pub i32, pub i32);
+#[derive(Debug, Clone, Copy, Eq, Hash, PartialOrd, Ord)]
+struct Pos {
+    x: i32,
+    y: i32,
+    direction: Direction,
+    count: u32,
+}
+impl PartialEq for Pos {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
 
 fn is_in_bounds(position: (i32, i32), width: usize, height: usize) -> bool {
     position.0 >= 0 && position.1 >= 0 && position.0 < width as i32 && position.1 < height as i32
 }
 
 fn get_successors(grid: &Array2D<u32>, pos: Pos) -> Vec<(Pos, u32)> {
-    Direction::iter().filter_map(|direction| {
-                // if direction == prev_direction.opposite() {
-                //     return None;
-                // }
-                // let direction_count = if direction == prev_direction {
-                //     prev_count + 1
-                // } else {
-                //     0
-                // };
+    Direction::iter()
+        .filter_map(|direction| {
+            if direction == pos.direction.opposite() {
+                return None;
+            }
+            let direction_count = if direction == pos.direction {
+                pos.count + 1
+            } else {
+                0
+            };
 
-                // go max 3 times in the same direction
-                // if direction_count > 2 {
-                //     return None;
-                // }
-                let delta = direction.delta();
-                let new_pos = (
-                    pos.0 as i32 + delta.0,
-                    pos.1 as i32 + delta.1,
-                );
-                is_in_bounds(new_pos, grid.num_columns(), grid.num_rows()).then(|| (Pos(new_pos.0, new_pos.1), *grid.get(new_pos.0 as usize, new_pos.1 as usize).expect("found heat loss")))
+            // go max 3 times in the same direction
+            if direction_count > 2 {
+                return None;
+            }
+            let delta = direction.delta();
+            let new_pos = (pos.x + delta.0, pos.y + delta.1);
+            is_in_bounds(new_pos, grid.num_columns(), grid.num_rows()).then(|| {
+                (
+                    Pos {
+                        x: new_pos.0,
+                        y: new_pos.1,
+                        direction,
+                        count: direction_count,
+                    },
+                    *grid
+                        .get(new_pos.0 as usize, new_pos.1 as usize)
+                        .expect("found heat loss"),
+                )
             })
+        })
         .collect()
 }
 
@@ -75,24 +104,54 @@ fn part1(input: &str) -> Result<u32, Error> {
         .collect();
 
     let grid = Array2D::from_rows(&lines)?;
-    // let mut status_grid = Array2D::filled_with(0, grid.num_rows(), grid.num_columns());
 
     let width = grid.num_columns();
     let height = grid.num_rows();
 
-    let position = Pos(0, 0);
-    let target = Pos(width as i32 - 1, height as i32 - 1);
+    let position = Pos {
+        x: 0,
+        y: 0,
+        direction: NORTH,
+        count: 0,
+    };
+    let target = Pos {
+        x: width as i32 - 1,
+        y: height as i32 - 1,
+        direction: EAST,
+        count: 0,
+    };
 
-    let result = astar(
+    let (path, heat_loss) = astar(
         &position,
         |p| get_successors(&grid, *p),
         // manhattan distance heuristic
-        |p| ((p.0 - target.0).abs() + (p.1 - target.1).abs()) as u32,
-        |p| *p == target
-    ).expect("found path");
-    
-    println!("Found path with length {}: {:?}", result.1, result.0);
-    Ok(result.1)
+        |p| ((p.x - target.x).abs() + (p.y - target.y).abs()) as u32,
+        |p| *p == target,
+    )
+    .expect("found path");
+
+    // let start_heat_loss = grid.get(0, 0).expect("found heat loss");
+    println!("Found path with length {}: {:?}", heat_loss, path);
+
+    for y in 0..height as i32 {
+        for x in 0..width as i32 {
+            let pos = path.iter().find(|p| p.x == x && p.y == y);
+            if pos.is_some() {
+                print!("{}", pos.unwrap().direction.char());
+            } else {
+                print!("{}", grid.get(x as usize, y as usize).unwrap());
+            }
+        }
+        println!();
+    }
+
+    let loss_sum = path
+        .iter()
+        .map(|p| grid.get(p.x as usize, p.y as usize).unwrap())
+        .sum::<u32>();
+    println!("Loss sum: {}", loss_sum);
+
+    Ok(heat_loss)
 }
 
 fn main() {
