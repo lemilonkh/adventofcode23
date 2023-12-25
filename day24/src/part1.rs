8a760397c5be7ffba5a95b7c547ab052d9045756
Milan Gruner <milangruner@gmail.com>
2023-12-25 01:55:31 +0100

    Solve day 24
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

---
+++ day24/src/part1.rs
@@ -0,0 +1,135 @@
+use itertools::Itertools;
+use std::{
+    ops::{Add, Mul},
+    str::FromStr,
+};
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{char, digit1, space1},
+    combinator::{map_res, opt, recognize},
+    multi::separated_list1,
+    sequence::{pair, preceded, separated_pair},
+    IResult,
+};
+
+#[derive(Debug, Clone, Copy)]
+struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, f: f64) -> Vec3 {
+        Vec3 {
+            x: self.x * f,
+            y: self.y * f,
+            z: self.z * f,
+        }
+    }
+}
+
+impl Add<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, v: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + v.x,
+            y: self.y + v.y,
+            z: self.z + v.z,
+        }
+    }
+}
+
+fn line_parser(i: &str) -> IResult<&str, (Vec3, Vec3)> {
+    separated_pair(vec3_parser, pair(tag(" @"), space1), vec3_parser)(i)
+}
+
+fn vec3_parser(i: &str) -> IResult<&str, Vec3> {
+    let (i, numbers) = separated_list1(pair(char(','), space1), int_parser)(i)?;
+    Ok((
+        i,
+        Vec3 {
+            x: numbers[0],
+            y: numbers[1],
+            z: numbers[2],
+        },
+    ))
+}
+
+fn int_parser(i: &str) -> IResult<&str, f64> {
+    map_res(
+        recognize(preceded(opt(tag("-")), digit1)),
+        FromStr::from_str,
+    )(i)
+}
+
+fn part1(input: &str, axis_min: f64, axis_max: f64) -> usize {
+    let stones: Vec<(Vec3, Vec3)> = input
+        .lines()
+        .map(|line| line_parser(line).expect("valid input").1)
+        .collect();
+
+    stones
+        .iter()
+        .tuple_combinations()
+        .filter(|(&(pa1, va), &(pb1, vb))| {
+            println!("Hailstone A: {:?} @ {:?}", pa1, va);
+            println!("Hailstone B: {:?} @ {:?}", pb1, vb);
+
+            let pa2 = pa1 + va;
+            let pb2 = pb1 + vb;
+
+            // calculate line-line intersection with 2 points on each line
+            let ta = ((pa1.x - pb1.x) * (pb1.y - pb2.y) - (pa1.y - pb1.y) * (pb1.x - pb2.x))
+                / ((pa1.x - pa2.x) * (pb1.y - pb2.y) - (pa1.y - pa2.y) * (pb1.x - pb2.x));
+            let tb = ((pa1.x - pb1.x) * (pa1.y - pa2.y) - (pa1.y - pb1.y) * (pa1.x - pa2.x))
+                / ((pa1.x - pa2.x) * (pb1.y - pb2.y) - (pa1.y - pa2.y) * (pb1.x - pb2.x));
+
+            if ta < 0.0 {
+                println!("Hailstones' paths crossed in the past for hailstone A.");
+                return false;
+            }
+            if tb < 0.0 {
+                println!("Hailstones' paths crossed in the past for hailstone B.");
+                return false;
+            }
+
+            let cross = pa1 + va * ta;
+            println!("Crossing at {:?}", cross);
+            cross.x >= axis_min && cross.x <= axis_max && cross.y >= axis_min && cross.y <= axis_max
+        })
+        .count()
+}
+
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input, 200_000_000_000_000.0, 400_000_000_000_000.0);
+    dbg!(output);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let result = part1(include_str!("input1_test.txt"), 7.0, 27.0);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn parse_stone() {
+        let (i, (a, b)) = line_parser("20, 19, 15 @  1, -5, -3").expect("correctly parsed");
+        assert_eq!(i, "");
+        assert_eq!(a.x, 20.0);
+        assert_eq!(a.y, 19.0);
+        assert_eq!(a.z, 15.0);
+
+        assert_eq!(b.x, 1.0);
+        assert_eq!(b.y, -5.0);
+        assert_eq!(b.z, -3.0);
+    }
+}
---
+++ day24/src/part2.rs
@@ -0,0 +1,139 @@
+use itertools::Itertools;
+use ndarray::prelude::*;
+use ndarray_linalg::Solve;
+use std::str::FromStr;
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{char, digit1, space1},
+    combinator::{map_res, opt, recognize},
+    multi::separated_list1,
+    sequence::{pair, preceded, separated_pair},
+    IResult,
+};
+
+#[derive(Debug, Clone, Copy)]
+struct Vec3 {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+fn line_parser(i: &str) -> IResult<&str, (Vec3, Vec3)> {
+    separated_pair(vec3_parser, pair(tag(" @"), space1), vec3_parser)(i)
+}
+
+fn vec3_parser(i: &str) -> IResult<&str, Vec3> {
+    let (i, numbers) = separated_list1(pair(char(','), space1), int_parser)(i)?;
+    Ok((
+        i,
+        Vec3 {
+            x: numbers[0],
+            y: numbers[1],
+            z: numbers[2],
+        },
+    ))
+}
+
+fn int_parser(i: &str) -> IResult<&str, i64> {
+    map_res(
+        recognize(preceded(opt(tag("-")), digit1)),
+        FromStr::from_str,
+    )(i)
+}
+
+fn find_speed(speeds: Vec<(i64, i64)>) -> i64 {
+    let min_speed = speeds.iter().map(|s| s.1).min().unwrap();
+    let max_speed = speeds.iter().map(|s| s.1).max().unwrap();
+    let possibilities = vec![true; (max_speed - min_speed + 1) as usize];
+
+    speeds
+        .iter()
+        .tuple_combinations()
+        .fold(possibilities, |mut possible, ((p1, v1), (p2, v2))| {
+            if v1 == v2 {
+                let delta = (p1 - p2).abs();
+                for i in 0..possible.len() {
+                    let cross = i as i64 + min_speed - *v2;
+                    if cross != 0 && possible[i] && delta % cross != 0 {
+                        possible[i] = false;
+                    }
+                }
+            }
+            possible
+        })
+        .into_iter()
+        .position(|s| s) // find first true
+        .unwrap() as i64
+        + min_speed
+}
+
+fn part2(input: &str) -> i64 {
+    let stones: Vec<(Vec3, Vec3)> = input
+        .lines()
+        .map(|line| line_parser(line).expect("valid input").1)
+        .collect();
+
+    // determine possible velocity of thrown rock
+    let speed_x = find_speed(stones.iter().map(|(p, v)| (p.x, v.x)).collect()) as f64;
+    let speed_y = find_speed(stones.iter().map(|(p, v)| (p.y, v.y)).collect()) as f64;
+    let speed_z = find_speed(stones.iter().map(|(p, v)| (p.z, v.z)).collect()) as f64;
+
+    // construct matrix and vector for solver
+    let matrix: Array2<f64> = array![
+        [1.0, 0.0, 0.0, speed_x - stones[0].1.x as f64, 0.0],
+        [0.0, 1.0, 0.0, speed_y - stones[0].1.y as f64, 0.0],
+        [0.0, 0.0, 1.0, speed_z - stones[0].1.z as f64, 0.0],
+        [1.0, 0.0, 0.0, 0.0, speed_x - stones[1].1.x as f64],
+        [0.0, 1.0, 0.0, 0.0, speed_y - stones[1].1.y as f64],
+    ];
+    let vector: Array1<f64> = array![
+        stones[0].0.x as f64,
+        stones[0].0.y as f64,
+        stones[0].0.z as f64,
+        stones[1].0.x as f64,
+        stones[1].0.y as f64,
+    ];
+
+    let solution = matrix.solve_into(vector).expect("found solution");
+    println!(
+        "Found solution! Throw rock from ({}, {}, {}) with speed ({}, {}, {})",
+        solution[0], solution[1], solution[2], speed_x, speed_y, speed_z
+    );
+
+    (solution[0] + solution[1] + solution[2]).round() as i64
+}
+
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part2(input);
+    dbg!(output);
+}
+
+#[link(name = "lapack")]
+extern "C" {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let result = part2(include_str!("input1_test.txt"));
+        // 47 is given in example but this seems to work too. Maybe too
+        // unconstrained, also not quite an integer solution
+        assert_eq!(result, 75);
+    }
+
+    #[test]
+    fn parse_stone() {
+        let (i, (a, b)) = line_parser("20, 19, 15 @  1, -5, -3").expect("correctly parsed");
+        assert_eq!(i, "");
+        assert_eq!(a.x, 20);
+        assert_eq!(a.y, 19);
+        assert_eq!(a.z, 15);
+
+        assert_eq!(b.x, 1);
+        assert_eq!(b.y, -5);
+        assert_eq!(b.z, -3);
+    }
+}
